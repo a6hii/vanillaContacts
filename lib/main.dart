c143b904fe26befe81616b69d2ed0da5d33125c1
Abhishek <abhikr_dev@protonmail.com>
2022-06-12 08:42:03 +0530

    Step-1

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Contacts',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      routes: {
+        '/new-contact': (context) => const AddNewContactView(),
+      },
+    ),
+  );
+}
+
+class Contact {
+  final String name;
+
+  Contact({required this.name});
+}
+
+class ContactBook {
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+  final List<Contact> _contacts = [];
+  //we are going to use listview which needs itemcount:
+  int get length => _contacts.length;
+
+  void add({required Contact contact}) {
+    _contacts.add(contact);
+  }
+
+  void remove({required Contact contact}) {
+    _contacts.remove(contact);
+  }
+
+  //a function to retrieve contacts by index
+  Contact? contact({required int atIndex}) =>
+      _contacts.length > atIndex ? _contacts[atIndex] : null;
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final contactBook = ContactBook();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index)!;
+          return ListTile(
+            title: Text(contact.name),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.of(context).pushNamed('/new-contact');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AddNewContactView extends StatefulWidget {
+  const AddNewContactView({super.key});
+
+  @override
+  State<AddNewContactView> createState() => _AddNewContactViewState();
+}
+
+class _AddNewContactViewState extends State<AddNewContactView> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add New Contact'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: 'Enter contact name...',
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              final contact = Contact(name: _controller.text);
+              ContactBook().add(contact: contact);
+              Navigator.of(context).pop();
+            },
+            child: const Text("Add contact"),
+          ),
+        ],
+      ),
+    );
+  }
+}
